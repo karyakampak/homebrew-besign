67d7a0051c02872e87c7f9c022a03627c6086a3a
karyakampak <karyadi.dk@gmail.com>
2024-09-16 07:54:31 +0700

    setup 1

---
+++ Formula/besign.rb
@@ -0,0 +1,19 @@
+class Myprogram < Formula
+  desc "My awesome program"
+  homepage "https://github.com/karyakampak/besign"
+  url "https://github.com/karyakampak/besign/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "8dcc4836dea4e249c9712e5e3eadb6108b2a97b78fb796b42dbc193030ee673b"
+  license "MIT"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/dksign", "--version"
+  end
+end
