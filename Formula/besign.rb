71f4c877ed2c7c44864aae62247bd50bc5e81213
karyakampak <karyadi.dk@gmail.com>
2024-09-16 09:53:12 +0700

    Add besign formula v2

--- Formula/besign.rb
+++ Formula/besign.rb
@@ -6,10 +6,24 @@ class Besign < Formula
   license "MIT"
 
   depends_on "cmake" => :build
+  depends_on "python" # Python 3 is installed via Homebrew and will be available for CMake
+  depends_on "opencv"
+  depends_on "qrencode"
+  depends_on "openssl"
+  depends_on "curl"
+  depends_on "nlohmann-json"
+  depends_on "uuid"
 
   def install
+    # Install Python dependencies directly
+    # Ensure pip is available
+    python_version = Language::Python.major_minor_version "python3"
+    system "pip#{python_version}", "install", "fitz"
+    system "pip#{python_version}", "install", "qrcode"
+    system "pip#{python_version}", "install", "Pillow"
+    
     cd "build" do
-      system "cmake", "..", *std_cmake_args
+      system "cmake", "..", "-DCMAKE_BUILD_TYPE=Release", *std_cmake_args
       system "make"
       system "make", "install"
     end
