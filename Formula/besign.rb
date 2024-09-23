7ea9438ec3fa892d7c1d29be9ae355514f55a67e
karyakampak <karyadi.dk@gmail.com>
2024-09-23 20:07:26 +0700

    New Release Formula release 1

--- Formula/besign.rb
+++ Formula/besign.rb
@@ -1,8 +1,8 @@
 class Besign < Formula
   desc "Program for make signature object on pdf"  
   homepage "https://github.com/karyakampak/homebrew-besign"
-  url "https://github.com/karyakampak/homebrew-besign/archive/refs/tags/v1.0-release.tar.gz"
-  sha256 "bf158902cc886587597e1bf7a047b4493ff0200be4f3347b82edad1a2375ee45"
+  url "https://github.com/karyakampak/homebrew-besign/archive/refs/tags/v1.0.0-release.tar.gz"
+  sha256 "0193d0694a274ec5fffae8531929aa7bbc520eba5639daf26eeccbb9f3034535"
   license "MIT"
 
   depends_on "cmake" => :build
