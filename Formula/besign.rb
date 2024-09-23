bf7958532b3528212e91f805d6dc4864ee939455
karyakampak <karyadi.dk@gmail.com>
2024-09-23 19:49:32 +0700

    New Release Formula

--- Formula/besign.rb
+++ Formula/besign.rb
@@ -1,8 +1,8 @@
 class Besign < Formula
   desc "Program for make signature object on pdf"  
   homepage "https://github.com/karyakampak/homebrew-besign"
-  url "https://github.com/karyakampak/homebrew-besign/archive/refs/tags/v1.0.6.tar.gz"
-  sha256 "4a26ffd95046dc748222aeb49cddc23eedc94ecb958521083455db25d0ac570c"
+  url "https://github.com/karyakampak/homebrew-besign/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "093038b3e1191d73d5fdf5a85363f774ee714e07a16a6680af88f6e69fca3667"
   license "MIT"
 
   depends_on "cmake" => :build
@@ -16,19 +16,8 @@ class Besign < Formula
 
 
   def install
-    python3 = Formula["python@3"].opt_bin/"python3"
-    pip = Formula["python@3"].opt_bin/"pip3"
-    venv_path = buildpath/"venv"
-
-    # Create and activate a virtual environment
-    system "#{python3}", "-m", "venv", venv_path
-    system "#{venv_path}/bin/pip", "install", "--upgrade", "setuptools", "wheel", "Cython", "numpy", "pymupdf", "qrcode[pil]", "pillow"
 
     cd "build" do
-      # Set environment variables to use the virtual environment
-      ENV["PYTHONPATH"] = "#{venv_path}/lib/python#{Formula["python@3"].version.major_minor}/site-packages"
-      ENV.prepend_path "PATH", "#{venv_path}/bin"
-
       system "cmake", "..", *std_cmake_args
       system "make"
       system "make", "install"
