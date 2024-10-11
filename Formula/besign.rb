049fdd5eb3e1dae91ad57ef53c40787c042ed5b5
karyakampak <karyadi.dk@gmail.com>
2024-10-11 17:36:49 +0700

    Dev 8

--- Formula/besign.rb
+++ Formula/besign.rb
@@ -1,8 +1,8 @@
 class Besign < Formula
   desc "Program for make signature object on pdf"  
   homepage "https://github.com/karyakampak/homebrew-besign"
-  url "https://github.com/karyakampak/homebrew-besign/archive/refs/tags/v1.0.3-dev.tar.gz"
-  sha256 "7dd93409d254f499d4a45d17360c0be7db6297e4401d5e3e12c04b4f39e19f7f"
+  url "https://github.com/karyakampak/homebrew-besign/archive/refs/tags/v1.0.4-dev.tar.gz"
+  sha256 "aa1f4ea4e6f0d1de4a4829fb1b1667d073b1bb98e4b82b82671c4668a8caa4cb"
   license "MIT"
 
   depends_on "cmake" => :build
